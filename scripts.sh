83e62785d7d095b3d5eac7497e254f8db98d7ea3
amit <amitmainemer@gmail.com>
2024-05-08 02:46:17 +0300

    v1.1.1

--- scripts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-alias op="cd /c/Projects/onePiecePyPlayer/OnePiece && python main.py $1"
-alias ju="cd /c/Projects/onePiecePyPlayer/Jujutsu && python main.py $1"
-alias anima="cd /c/Projects/onePiecePyPlayer && python anima.py $1"
